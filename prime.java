46466d720ce7ee982cb62d200f8d5ab2fb90361b
Mike Le <Mike.lemanh@gmail.com>
2026-05-20 18:33:44 +0930

    Finish Homework

---
+++ prime.java
@@ -0,0 +1,45 @@
+package part2;
+
+import java.util.Scanner;
+
+public class prime {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        boolean result = isPrime(number);
+        
+        System.out.println(result);
+        
+        scanner.close();
+    }
+ 
+    public static boolean isPrime(int number) {
+        
+        if (number <= 1) {
+            return false;
+        }
+        
+        // 2 and 3 are prime numbers
+        if (number == 2 & number == 3) {
+            return true;
+        }
+        
+        // all even numbers are not prime, except 2.
+        if (number % 2 == 0) {
+            return false;
+        }
+        
+        // if number is divisible by i, it is not prime
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
