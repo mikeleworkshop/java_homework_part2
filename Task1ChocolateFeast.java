b3f62751219a9888f5c25d31e5d22a6902ef7fc0
Mike Le <Mike.lemanh@gmail.com>
2026-06-02 12:15:48 +0000

    Update Task1

--- Task1ChocolateFeast.java
+++ Task1ChocolateFeast.java
@@ -2,42 +2,36 @@ package part5;
 
 public class Task1ChocolateFeast {
 	
-	public static int solution1(int initialMoney, int cost, int wrappersNeed) {
-        int numBars = initialMoney / cost;
-        int numEaten = numBars;
-        int numWrappers = numBars;
-        
-        while (numWrappers >= wrappersNeed) {
-            int newBars = numWrappers / wrappersNeed;
-            System.out.println("numWrappers: " + numWrappers);
-	        System.out.println("end");
-	        
-            numEaten = numEaten + newBars;
-            
-            int remaining = numWrappers % wrappersNeed;
-            System.out.println("remaining: " + remaining);
-	        System.out.println("end");
-	        
-            numWrappers = remaining + newBars;
-            System.out.println("numWrappers: " + numWrappers);
-	        System.out.println("end");
-        }
-        
-        return numEaten;
-    }
+	public static int solution1 (int money, int cost, int wrappersNeed) {
+		int numBars = money / cost;
+		int numWrappers = numBars;
+		
+		while (numWrappers >= wrappersNeed) {
+			int newBars = numWrappers / wrappersNeed;
+			numBars += newBars;
+			
+			int remaining = numWrappers % wrappersNeed;
+			remaining += newBars;
+			numWrappers = remaining;
+		}
+		
+		return numBars;
+		
+	}
 	
-	public static int solution2(int initialMoney, int cost, int wrappersNeed) {
-        int numBars = initialMoney / cost;
-        
-        if (wrappersNeed > 1 && numBars >= wrappersNeed) {
-        	numBars += (numBars - 1) / (wrappersNeed - 1);
-        }
-        
-        return numBars;
+	public static int solution2 (int money, int cost, int wrappersNeed) {
+		int numBars = money / cost;
+		int numWrappers = numBars;
+		
+		if (numWrappers > 1 && numWrappers >= wrappersNeed) {
+			numBars += (numBars - 1) / (wrappersNeed -1);
+		}
+		
+		return numBars;
 	}
 	
 	public static void main(String[] args) {
-		        System.out.println(solution2(15, 3, 2)); // Expected: 6
+		        System.out.println(solution1(126, 2, 4));
 		      
 	}
 }
