f8a9dccd4e5122b9cbc59837296b99a35abfe47c
Mike Le <Mike.lemanh@gmail.com>
2026-07-04 19:48:53 +0930

    Fix code logic and output

--- Dominator.java
+++ Dominator.java
@@ -2,37 +2,47 @@ public class Dominator {
 	
     public static void main(String[] args) {
         int[] A = {3, 4, 3, 2, 3, -1, 3, 3};
-        int result = solution(A);
-        System.out.println("The dominator of array is " + A[result]); 
+        int[] result = solution(A);
+        
+        System.out.println("The dominator of array is ");
+        for (int i = 0; i < result.length; i++) { 
+	        System.out.print(result[i] + " ");
+        }
     }
 
-    public static int solution(int[] A) {
+    public static int[] solution(int[] A) {
             int N = A.length;
+
+			int[] result = new int[N - 1];
+			int[] error = new int[1];
+			error[0] = -1;
+			
             if (N == 0) {
-                return -1;
+				return error;
             }
 
-            int target = A[0];
-            int count = 0;
+            int candidate = A[0];
+            int votes = 0;
             int occurrences = 0;
             int index = 0;
             for (int i = 0; i < N; i++) {
-                if (count == 0) {
-                    target = A[i];
+                if (votes == 0) {
+                    candidate = A[i];
                 }
-                if (A[i] == target) {
-                    count++;
+                if (A[i] == candidate) {
+                    votes++;
                     occurrences++;
-                    index = i;
+                    result[index] = i;
+                    index++;
                 } else {
-                    count--;
+                    votes--;
                 }
             }
             
             if (occurrences > N / 2.0) {
-            	return index;
+            	return result;
             } else {
-            	return -1;
-            }   
+            	return error;
+            }
         }
 }
